e475ff37da25da43b48f8bad74d59b2d2228f931
Brian Cline <brian.cline@gmail.com>
2013-10-29 16:22:32 -0500

    Initial commit with Sam's original code and MIT License

---
+++ README.md
@@ -0,0 +1,38 @@
+[CRM-114][1] Python Module
+==========================
+
+This project is a Python module to interact with [The CRM-114 Discriminator][2],
+which handles learning and classification of text streams. While written and
+used primarily in spam classification, CRM-114 handles text streams of logs,
+data, etc. just as well with recorded accuracy rates exceeding 99.9%. A wide
+variety of methods can be used with CRM-114, namely regular expressions,
+approximate regular expressions, Hidden Markov Model, Orthogonal Sparse Bigrams
+(OSB), winnow, general correlation, K-Nearest-Neighbor, and bit entropy.
+
+Originally crafted by [Sam Deane][3] of [Elegant Chaos][4] and [Born Sleepy][5],
+with ongoing improvements and maintenance by [Brian Cline][6].
+
+
+
+Examples
+--------
+
+    print('TODO')
+
+
+
+License
+-------
+
+Released under the MIT License. See LICENSE file for details.
+
+Original code licensed under GPLv2, re-licensed 29 October 2013 by Sam Deane
+under the MIT license for further curation, maintenance, and packaging.
+
+
+  [1]: http://en.wikipedia.org/wiki/CRM_114_(fictional_device)
+  [2]: http://crm114.sourceforge.net/
+  [3]: https://github.com/samdeane
+  [4]: http://www.elegantchaos.com/
+  [5]: http://bornsleepy.com/
+  [6]: https://github.com/briancline
---
+++ crm.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+
+"""
+$Id: crm.py 158 2005-06-07 17:45:47Z sam $
+
+Python wrapper classes for the CRM114 Discriminator (http://crm114.sourceforge.net/).
+
+Requires the crm command to be installed and in your command path.
+
+The latest version of this file can be obtained from the Elegant Chaos subversion server (user=guest, pass=guest) at:
+    $URL: http://source.elegantchaos.com/projects/com/elegantchaos/libraries/python/crm.py $
+
+This module provides a very simplified interface to crm114. It does not attempt to expose all of crm114's power, instead it
+tries to hide almost all of the gory details.
+
+To use the module, create an instance of the Classifier class, giving it a path (where to store the data files), and a list
+of category strings (these are the "labels" to classify the text with).
+
+e.g:
+    c = Classifier("/path/to/my/data", ["good", "bad"])
+
+To teach the classifier object about some text, call the learn method passing in a category (on of the ones that you provided originally),
+and the text.
+
+e.g:
+    c.learn("good", "some good text")
+    c.learn("bad", "some bad text")
+    
+To find out what the classifier things about some text, call the classify method passing in the text. The result of this
+method is a pair - the first item being the category best matching the text, and the second item being the probability of the match.
+
+e.g:
+    (classification, probability) = c.classify("some text")
+
+TODO: use proper path separator variable in the regular expression instead of assuming that it's a slash
+    
+"""
+
+__version__ = "1.0.0a1"
+
+__license__ = """
+Copyright (C) 2005 Sam Deane.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import os;
+import string;
+
+#constants
+
+kCrmPath = "crm"
+
+kClassificationType = "<osb unique microgroom>"
+kClassificationExtension = ".css"
+
+kLearnCommand = " '-{ learn %s ( %s ) }'"
+kClassifyCommand = " '-{ isolate (:stats:); classify %s ( %s ) (:stats:); match [:stats:] (:: :best: :prob:) /Best match to file .. \(%s\/([[:graph:]]+)\\%s\) prob: ([0-9.]+)/; output /:*:best:\\t:*:prob:/ }'"
+
+
+# wrapper for crm114
+class Classifier:
+
+    def __init__( self, path, categories = [] ):
+        self.categories = categories
+        self.path = path
+        self.makeFiles()
+        
+    # learn the classifier what category some new text is in 
+    def learn( self, category, text ):
+        command = kCrmPath + ( kLearnCommand % ( kClassificationType, os.path.join( self.path, category + kClassificationExtension ) ) )
+
+        pipe = os.popen( command, 'w' )
+        pipe.write( text )
+        pipe.close()
+    
+    # ask the classifier what category best matches some text   
+    def classify( self, text ):
+        path = string.replace(self.path, "/", "\\/") # need to escape path separator for the regexp matching
+        command = kCrmPath + ( kClassifyCommand % (kClassificationType, self.getFileListString(), path, kClassificationExtension) )
+        (fin, fout) = os.popen2( command )
+        fin.write( text )
+        fin.close()
+        list = string.split(fout.readline())
+        fout.close()
+        if list == None:
+            return ("", 0.0)
+        else:
+            category = list[0]
+            probability = float(list[1])
+            return (category, probability)
+
+    # ensure that data files exist, by calling learn with an empty string
+    def makeFiles( self ):
+        # make directory if necessary
+        if not os.path.exists( self.path ):
+                os.mkdir( self.path )
+
+        # make category files
+        for category in self.categories:
+            self.learn( category, "" )
+            
+            
+    # return a list of classification files
+    def getFileList( self ):
+        
+        # internal method to build a file path given a category
+        def getFilePath( file ):
+            return os.path.join( self.path, file + kClassificationExtension )
+        
+        # return list of all category paths
+        return map( getFilePath, self.categories )
+        
+
+    # return a list of classification files as a string
+    def getFileListString( self ):
+        return string.join( self.getFileList(), " " )
+
+    # perform some self tests   
+    def test( self ):
+        print self.getFileList()
+            self.learn( "good", "this is a test" )
+            self.learn( "bad", "this is very bad" )
+            print "class was: %s, prob was:%f" % ( self.classify( "this is a test" ) )
+        
+        
+if __name__ == "__main__":
+    # perform a simple test
+    c = Classifier( "test/data", [ "good", "bad" ] )
+    c.test()
