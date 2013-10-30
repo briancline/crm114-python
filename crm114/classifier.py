ae83fcceaced0480cff62ef0a0f779186dd913e1
Brian Cline <brian.cline@gmail.com>
2013-10-30 12:55:26 -0500

    Make all code PEP-8 compliant, expand tests
    * Add docstrings where comments were previously used
    * De-camelfy methods
    * Update a few comments
    * Create all data directory parents if necessary
    * Expand basic test case quite a bit (it's still basic by any measure
      of learning), and format output to fit each classification test on
      a single line in a sorted list for easy reading
    * Clear test data files between runs
    
    Closes-Bug: #1

--- crm114/classifier.py
+++ crm114/classifier.py
@@ -1,71 +1,89 @@
-import os;
-import string;
+import os
+import string
 
-#constants
+CRM_BINARY = 'crm'
 
-kCrmPath = "crm"
+CLASSIFICATION_TYPE = '<osb unique microgroom>'
+CLASSIFICATION_EXT = '.css'
 
-kClassificationType = "<osb unique microgroom>"
-kClassificationExtension = ".css"
+LEARN_CMD = " '-{ learn %s ( %s ) }'"
+CLASSIFY_CMD = " '-{ isolate (:stats:);" \
+               " classify %s ( %s ) (:stats:);" \
+               " match [:stats:] (:: :best: :prob:)" \
+               " /Best match to file .. \(%s\/([[:graph:]]+)\\%s\)" \
+               " prob: ([0-9.]+)/;" \
+               " output /:*:best:\\t:*:prob:/ }'"
 
-kLearnCommand = " '-{ learn %s ( %s ) }'"
-kClassifyCommand = " '-{ isolate (:stats:); classify %s ( %s ) (:stats:); match [:stats:] (:: :best: :prob:) /Best match to file .. \(%s\/([[:graph:]]+)\\%s\) prob: ([0-9.]+)/; output /:*:best:\\t:*:prob:/ }'"
 
-
-# wrapper for crm114
 class Classifier:
-
-    def __init__( self, path, categories = [] ):
-        self.categories = categories
+    def __init__(self, path, categories=None):
+        """ Wrapper class for the CRM-114 Discriminator. """
+        self.categories = categories if categories else []
         self.path = path
-        self.makeFiles()
-        
-    # learn the classifier what category some new text is in 
-    def learn( self, category, text ):
-        command = kCrmPath + ( kLearnCommand % ( kClassificationType, os.path.join( self.path, category + kClassificationExtension ) ) )
+        self.create_files()
 
-        pipe = os.popen( command, 'w' )
-        pipe.write( text )
+    # learn the classifier what category some new text is in
+    def learn(self, category, text):
+        """ Feed the classifier some new text and a known classification for
+        it, in order to improve subsequent categorizations. """
+
+        command = CRM_BINARY + (LEARN_CMD % (CLASSIFICATION_TYPE,
+                                             os.path.join(self.path, category +
+                                                          CLASSIFICATION_EXT)))
+
+        pipe = os.popen(command, 'w')
+        pipe.write(text)
         pipe.close()
-    
-    # ask the classifier what category best matches some text   
-    def classify( self, text ):
-        path = string.replace(self.path, os.sep, "\\%s" % os.sep) # need to escape path separator for the regexp matching
-        command = kCrmPath + ( kClassifyCommand % (kClassificationType, self.getFileListString(), path, kClassificationExtension) )
-        (fin, fout) = os.popen2( command )
-        fin.write( text )
+
+    def classify(self, text):
+        """ Instructs the classifier to categorize the text, and return the
+        name of the category that best matches the text. """
+
+        # need to escape path separator for the regex matching
+        path = string.replace(self.path, os.sep, '\\%s' % os.sep)
+
+        command = CRM_BINARY + (CLASSIFY_CMD % (CLASSIFICATION_TYPE,
+                                                self.file_list_string(),
+                                                path,
+                                                CLASSIFICATION_EXT))
+        fin, fout = os.popen2(command)
+        #print('- Command:   %s' % command)
+        fin.write(text)
         fin.close()
+
         list = string.split(fout.readline())
         fout.close()
-        if list == None:
-            return ("", 0.0)
+
+        if list is None:
+            return ('', 0.0)
         else:
             category = list[0]
             probability = float(list[1])
             return (category, probability)
 
-    # ensure that data files exist, by calling learn with an empty string
-    def makeFiles( self ):
-        # make directory if necessary
-        if not os.path.exists( self.path ):
-                os.mkdir( self.path )
+    def create_files(self):
+        """ Ensures that the associated data files exist by learning an empty
+        string. """
 
-        # make category files
+        # Create directory if necessary
+        if not os.path.exists(self.path):
+            os.mkdirs(self.path)
+
+        # Create category files
         for category in self.categories:
-            self.learn( category, "" )
-            
-            
-    # return a list of classification files
-    def getFileList( self ):
-        
-        # internal method to build a file path given a category
-        def getFilePath( file ):
-            return os.path.join( self.path, file + kClassificationExtension )
-        
-        # return list of all category paths
-        return map( getFilePath, self.categories )
-        
+            self.learn(category, '')
+
+    def file_list(self):
+        """ Returns a list of classification files. """
+
+        # Builds a file path given a category name
+        def _file_path(category):
+            return os.path.join(self.path, category + CLASSIFICATION_EXT)
+
+        # Return list of all category paths
+        return map(_file_path, self.categories)
 
     # return a list of classification files as a string
-    def getFileListString( self ):
-        return string.join( self.getFileList(), " " )
+    def file_list_string(self):
+        """ Returns a list of classification files as a string. """
+        return ' '.join(self.file_list())
--- tests/basic.py
+++ tests/basic.py
@@ -1,14 +1,89 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+from __future__ import print_function
 import os
+from operator import itemgetter
 from crm114 import Classifier
 
+"""
+Performs an extremely basic set of tests by passing some fuzzily-defined words
+and phrases to the learner with an explicit sentiment, and runs an equally
+fuzzy and arbitrary set of texts through the classifier.
 
-if __name__ == "__main__":
-    # perform a simple test
+Hopefully it gets them right.
+"""
+
+
+if __name__ == '__main__':
     data_path = '%s%s%s' % (os.path.dirname(__file__), os.sep, 'data')
-    c = Classifier( data_path, [ "good", "bad" ] )
 
-    print c.getFileList()
-    c.learn( "good", "this is a test" )
-    c.learn( "bad", "this is very bad" )
-    print "class was: %s, prob was:%f" % ( c.classify( "this is a test" ) )
+    categories = ['good', 'bad']
+
+    print('Initializing classifier')
+    print('- Categories:  %s' % ', '.join(categories))
+    print('- Data path:   %s' % data_path)
+
+    c = Classifier(data_path, ['good', 'bad'])
+    for file_path in c.file_list():
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+        print('- Data file:   %s' % file_path)
+
+    c.create_files()
+    print('')
+
+    c.learn('good', 'this is a good test')
+    c.learn('good', 'pretty good')
+    c.learn('good', 'this is a VERY good test')
+    c.learn('good', 'this is a good test')
+    c.learn('good', 'this is a great test')
+    c.learn('good', 'awesome test')
+    c.learn('good', 'peachy test')
+    c.learn('good', 'love')
+    c.learn('good', 'hey')
+    c.learn('bad', 'a bad test')
+    c.learn('bad', 'pretty bad test')
+    c.learn('bad', 'this is a very bad test')
+    c.learn('bad', 'terrible test')
+    c.learn('bad', 'this is a treacherous test')
+    c.learn('bad', 'TREACHERY AT ITS FINEST')
+    c.learn('bad', 'this is a shit awful test')
+    c.learn('bad', 'hate')
+    c.learn('bad', 'HATED')  # Case-sensitive? Really?
+    c.learn('bad', 'made me care-vomit')
+    c.learn('bad', 'vomit')
+
+    classify_texts = ['this is a good test',
+                      'here is a pretty good test',
+                      'this is a bad test',
+                      'this is shit awful',
+                      'this is insanely awesome',
+                      'THIS IS SUCH AN AWESOME TEST',
+                      'I love this test so much.',
+                      'This is the finest test.',
+                      'HATED IT',
+                      "hey baby test, can I get your digits?",
+                      'I wanted to vomit',
+                      'Please, only the finest of your treachery',
+                      'this is a test',
+                      'a treacherous, terrible test that I hated ' +
+                      'and it made me vomit']
+
+    category_max = len(max(categories, key=len))
+    test_output_format = '%% 3.2f%%%%  %%%ds:  %%s' % category_max
+
+    test_results = []
+
+    for text in classify_texts:
+        category, probability = c.classify(text)
+        test_results.append({'category': category,
+                             'probability': probability,
+                             'text': text})
+
+    sorted_results = sorted(test_results, key=itemgetter('probability'),
+                            reverse=True)
+
+    for test in sorted_results:
+        print(test_output_format % (test['probability'] * 100.0,
+                                    test['category'],
+                                    test['text']))
